91642292c81eeee4e56acdc5b3c64217fdfba634
Cawa425 <kostan-sasha@bk.ru>
2020-05-31 09:09:58 +0300

    Рабочее v1

---
+++ Assets/MyScene/Scripts/BallController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+namespace MyScene.Scripts
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class BallController : MonoBehaviour
+    {
+        //Отступ камеры
+        [SerializeField] private Vector3 ballCameraOffSet = new Vector3(0f, -0.1f, 0.2f);
+        [SerializeField] private ARSessionOrigin sessionOrigin;
+        [SerializeField] private GameObject aRcam;    
+
+        //Направления броска
+        private Vector3 _startPosition;
+        private Vector3 _direction;
+
+
+        //данные для броска
+        public float throwForce = 100f;
+        private float _startTime;
+        private float _endTime;
+        private float _duration;
+        private bool _directionChosen;
+        private static Rigidbody _rb;
+
+        private float relaxTime;
+        public float mass;
+        void Awake()
+        {
+            Debug.Log("Мяч начал работу");
+            sessionOrigin = GameObject.Find("AR Session Origin").GetComponent<ARSessionOrigin>();
+            aRcam =  sessionOrigin.transform.Find("AR Camera").gameObject;
+            _rb = transform.gameObject.GetComponent<Rigidbody>();
+            transform.parent = aRcam.transform;
+            ResetBall();
+
+            Debug.Log(SaveSystem._Save.playerControll ? "Запущено первое управление" : "Запущено второе управление");
+
+            Debug.Log("Мяч готов");
+        }
+
+        void Update()
+        {
+            if (!_directionChosen && relaxTime<=0f)
+            {
+                if (!SaveSystem._Save.playerControll) SecondTapsControll();
+                else FirstCameraControll();
+                
+                
+                _startTime = 0.0f;
+                _duration = 0.0f;
+                _directionChosen = false;
+            }
+
+            relaxTime -= Time.deltaTime;
+            //After 5 sec autoreset ball
+            if (Time.time - _endTime >= 8 && Math.Abs(_endTime) > 0) ResetBall();
+        }
+
+
+        public void ResetBall()
+        {
+            _rb.mass = 0;
+            _rb.useGravity = false;
+            _rb.velocity = Vector3.zero;
+            _endTime = 0.0f;
+            _startPosition = Vector3.zero;
+            _direction = Vector3.zero;
+
+            var ball = transform;
+
+            //задает позицию и направление как у камеры
+            ball.position = aRcam.transform.position;
+            ball.LookAt(aRcam.transform.forward);
+
+            //сдвигает на пользовательское значение
+            ball.localPosition += ballCameraOffSet;
+            Debug.Log("Мяч вернулся");
+
+            relaxTime = 1f;
+        }
+
+        //управление камерой
+        private void FirstCameraControll()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _startPosition = transform.position; //позиция старта броска
+                _startTime = Time.time; //начало броска
+                _directionChosen = false; //направление выбрано
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                _endTime = Time.time; //Конец броска
+                _duration = _endTime - _startTime; //длительность броска
+                _direction = transform.position - _startPosition; //направление броска
+                _directionChosen = true;
+            }
+
+            if (_directionChosen)
+            {
+                _rb.mass = 1;
+                _rb.useGravity = true;
+                var force =new Vector3 (_direction.x *throwForce, _direction.y*throwForce  ,_direction.z/_duration);
+                _rb.AddForce(force);
+
+            }
+        }
+
+        //управление нажатиями
+        private void SecondTapsControll()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _startPosition = Input.mousePosition; //позиция старта броска
+                _startTime = Time.time; //начало броска
+                _directionChosen = false; //направление выбрано
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                _endTime = Time.time;
+                _duration = _endTime - _startTime;
+                _direction = Input.mousePosition - _startPosition;
+                Debug.Log($"Ты отжал кнопку в координатах {Input.mousePosition.x}x, {Input.mousePosition.y}y, {Input.mousePosition.z}z ");
+                _directionChosen = true;
+            }
+            
+            if (_directionChosen)
+            {
+                _rb.mass = mass;
+                _rb.useGravity = true;
+                var force = new Vector3 (_direction.x *throwForce, _direction.y*throwForce  ,_direction.z/_duration +_direction.y);
+                _rb.AddForce(force);
+
+            }
+        }
+    }
+}
---
+++ Assets/MyScene/Scripts/myUIManager.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MyScene.Scripts;
+using UnityEditor;
+using UnityEditor.Events;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+namespace Scenes.UX.MyScene
+{
+    [RequireComponent(typeof(ARCameraManager))]
+    public class myUIManager : MonoBehaviour
+    {
+        [SerializeField] [Tooltip("The ARCameraManager which will produce frame events.")]
+        private ARCameraManager mCameraManager;
+
+
+        /// <summary>
+        /// Get or set the <c>ARCameraManager</c>.
+        /// </summary>
+        public ARCameraManager cameraManager
+        {
+            get => mCameraManager;
+            set
+            {
+                if (mCameraManager == value)
+                    return;
+
+                if (mCameraManager != null)
+                    mCameraManager.frameReceived -= FrameChanged;
+
+                mCameraManager = value;
+
+                if (mCameraManager != null & enabled)
+                    mCameraManager.frameReceived += FrameChanged;
+            }
+        }
+
+        private const string KFadeOffAnim = "FadeOff";
+        private const string KFadeOnAnim = "FadeOn";
+
+        [SerializeField] private ARPlaneManager mPlaneManager;
+
+        public ARPlaneManager planeManager
+        {
+            get { return mPlaneManager; }
+            set { mPlaneManager = value; }
+        }
+
+        [SerializeField] private Animator mMoveDeviceAnimation;
+
+        public Animator moveDeviceAnimation
+        {
+            get => mMoveDeviceAnimation;
+            set => mMoveDeviceAnimation = value;
+        }
+
+        [SerializeField] private Animator mTapToPlaceAnimation;
+
+        public Animator tapToPlaceAnimation
+        {
+            get => mTapToPlaceAnimation;
+            set => mTapToPlaceAnimation = value;
+        }
+
+        static List<ARPlane> s_Planes = new List<ARPlane>();
+
+        private bool _mShowingTapToPlace = false;
+
+        private bool _mShowingMoveDevice = true;
+
+        void OnEnable()
+        {
+            if (mCameraManager != null)
+                mCameraManager.frameReceived += FrameChanged;
+
+            StartPlacing.onPlacedObject += PlacedObject;
+        }
+
+        void OnDisable()
+        {
+            if (mCameraManager != null)
+                mCameraManager.frameReceived -= FrameChanged;
+
+            StartPlacing.onPlacedObject -= PlacedObject;
+        }
+
+        void FrameChanged(ARCameraFrameEventArgs args)
+        {
+            if (PlanesFound() && _mShowingMoveDevice)
+            {
+                if (moveDeviceAnimation)
+                    moveDeviceAnimation.SetTrigger(KFadeOffAnim);
+
+                if (tapToPlaceAnimation)
+                    tapToPlaceAnimation.SetTrigger(KFadeOnAnim);
+
+                _mShowingTapToPlace = true;
+                _mShowingMoveDevice = false;
+            }
+        }
+
+        bool PlanesFound()
+        {
+            if (planeManager == null)
+                return false;
+
+            return planeManager.trackables.count > 0;
+        }
+
+        void PlacedObject()
+        {
+            if (_mShowingTapToPlace)
+            {
+                if (tapToPlaceAnimation)
+                    tapToPlaceAnimation.SetTrigger(KFadeOffAnim);
+
+                _mShowingTapToPlace = false;
+            }
+        }
+
+
+        [SerializeField] public List<GameObject> Settings;
+        public static List<GameObject> SettingsList;
+        public GameObject ResetButton;
+
+        public void Start()
+        {
+            SettingsList = Settings;
+            //Добовляем кнопке обработку возврата
+            ResetButton = SettingsList.First(x => x.name == "ResetBallButton" && x.CompareTag("Hud"));
+            var temp = GameObject.Find("Ball").GetComponent<BallController>();
+            ResetButton.GetComponent<Button>().onClick.AddListener(temp.ResetBall);
+        }
+    }
+}
